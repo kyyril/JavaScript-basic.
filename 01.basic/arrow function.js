552eaab719565943a118bb572badb057e8477cf4
KhairilRahman04 <KhairilRahman04>
2024-05-29 14:45:12 +0700

    arrow function

--- 01.basic/arrow function.js
+++ 01.basic/arrow function.js
@@ -1,11 +1,11 @@
 
-// const pangkat = function (n){
-//     console.log(n * n) 
-// }
-// pangkat(5) outout 25
-//ini function expression
+const pangkat = function (n){
+    console.log(n * n) 
+}
+pangkat(5) //outout 25
+// ini function expression
 
-
+console.log()
 
 const randomNum = (n) => { //ini arrow function
     console.log(Math.floor(Math.random() * n))
