fca4808937c80acb9947d2decd222e12e0be74c4
KhairilRahman04 <KhairilRahman04>
2024-05-31 23:46:43 +0700

    complete constructing. arry,objc,and function

--- new features in javascript/rest param.js
+++ new features in javascript/rest param.js
@@ -1,18 +1,19 @@
-// const angka = [1,2,3,4,5,6,7,8,9,10];
+//malas bikin parameter banyak? bisa pakai rest parameter aja
 
-// const hitung = angka.reduce((awal,akhir) => {
-//     return awal+akhir
-// });
-// console.log(hitung)
+//
+const angka = [1,2,3,4,5,6,7,8,9,10];
+const hitung = angka.reduce((awal,akhir) => {
+    return awal+akhir
+});
+console.log(hitung);//55
 
 
 //
 const nama = ['kiki','wahyu','jamal','cakmin','asep','amop','ucup'];
-
 const winner = (par1,par2,par3,...sisa) => {
     console.log(`${par1} juara 1`);
     console.log(`${par2} juara 2`);
     console.log(`${par3} juara 3`);
     console.log(`sisa peserta : ${sisa}`);
 };
-winner(...nama)
+winner(...nama);
